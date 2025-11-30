95ac52d68647bd2b8dfffcd2e61d4edcdba385bb
vinay <vinayydv2003@gmail.com>
2025-11-30 18:18:54 +0530

    Update server URL to production environment

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
 export const authDataContext=createContext()
 function AuthContext({children}) {
-const serverUrl="http://localhost:8000"
+const serverUrl="https://vybe-vnrp.onrender.com"
     let value={
         serverUrl
     }
