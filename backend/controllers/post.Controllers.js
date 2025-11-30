ef31c5dd1dbb8f009dac62fd468f3e1d25733598
Vinay <vinayydv2003@gmail.com>
2025-11-30 21:10:38 +0530

    import changed

--- backend/controllers/post.Controllers.js
+++ backend/controllers/post.Controllers.js
@@ -1,4 +1,4 @@
-import Post from "../models/Post.model.js"
+import Post from "../models/post.model.js"
 import uploadOnCloudinary from "../config/cloudinary.js"
 import { io } from "../index.js";
 import Notification from "../models/notification.model.js";
